c745cc8d6ff3bfcfa350819409676fbbb41ff120
Chris <Chris@CHRIS-PC>
2017-04-03 18:09:08 -0400

    Models Example

---
+++ EMA.Disaster.Recovery/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMA.Disaster.Recovery.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        public int CountyMunicipalityID { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public int Longitude { get; set; }
+        public int Lattitude { get; set; }
+        
+        public virtual CountyMunicipality CountyMunicipality { get; set; }
+    }
+}
---
+++ EMA.Disaster.Recovery/Models/CountyMunicipality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMA.Disaster.Recovery.Models
+{
+    public class CountyMunicipality
+    {
+        public int ID { get; set; }
+        public string County { get; set; }
+        public string Municipality { get; set; }
+    }
+}
