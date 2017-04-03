c3b159b7be1dbabe8acdf6e2904e4c03db9cbd36
Chris <Chris@CHRIS-PC>
2017-04-03 16:59:23 -0400

    Add project files.

---
+++ EMA.Disaster.Recovery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EMA.Disaster.Recovery.MvcApplication" Language="C#" %>
---
+++ EMA.Disaster.Recovery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EMA.Disaster.Recovery.Startup))]
+namespace EMA.Disaster.Recovery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
