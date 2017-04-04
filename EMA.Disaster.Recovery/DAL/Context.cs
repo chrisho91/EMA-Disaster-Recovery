d58676a13a5a4ae0f8862a1173bf9af3c17ad1a5
Chris <Chris@CHRIS-PC>
2017-04-04 10:15:50 -0400

    Context

--- EMA.Disaster.Recovery/DAL/Context.cs
+++ EMA.Disaster.Recovery/DAL/Context.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using EMA.Disaster.Recovery.Models;
 
 namespace EMA.Disaster.Recovery.DAL
 {
-    public class Context
+    public class Context : DbContext
     {
+        public Context() : base ("Context")
+        {
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<CountyMunicipality> CountyMunicipalities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
     }
 }
---
+++ EMA.Disaster.Recovery/DAL/DBInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EMA.Disaster.Recovery.Models;
+
+namespace EMA.Disaster.Recovery.DAL
+{
+    public class DBInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            var addresses = new List<Address>
+            {
+                new Address { }
+            };
+            base.Seed(context);
+        }
+    }
+}
